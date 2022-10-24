543d31ca7457ceb00f37e08aeb2af223c29834b2
techchee <ketyung@techchee.com>
2022-10-24 10:02:54 +0800

    update

--- src/collections_manage.rs
+++ src/collections_manage.rs
@@ -1,5 +1,4 @@
 use crate::*;
-use crate::ext::*;
 
 
 #[near_bindgen]
@@ -39,68 +38,20 @@ impl Contract {
         // use allowed caller check
         self.panic_if_its_not_allowed_caller();
 
-        Self::ext(env::current_account_id())
-        .with_static_gas(Gas(5*TGAS))
-        .create_collection_callback(acc_id, title, symbol, 
-        icon, base_uri, description, category,
-        total_tickets,tickets_sold, 
-        ticket_types,  attributes,template_type, contract_id);
-      
+        self.internal_create_collection(acc_id, 
+        title, symbol, icon, 
+        base_uri, description, category, 
+        total_tickets,tickets_sold,
+        ticket_types, attributes, 
+        template_type, contract_id);
+
+        self.date_updated = Some(env::block_timestamp());
     
     }
 
     
 
 }
-
-#[near_bindgen]
-impl Contract {
-
-    
-
-    #[private] // Public - but only callable by env::current_account_id()
-    pub fn create_collection_callback(&mut self,
-        acc_id : AccountId,     
-        title : String, 
-        symbol : String,
-        icon : Option<String>,
-        base_uri : Option<String>,
-        description : Option<String>,
-        category : Option<String>,
-        total_tickets : Option<u64>,
-        tickets_sold : Option<u64>,
-        ticket_types : Option<Vec<TicketType>>,
-        attributes : Option<Vec<Attribute>>,
-        template_type : Option<TicketTemplate>,
-        contract_id : Option<AccountId>,
-        #[callback_result] call_result: Result<bool, PromiseError>) {
-        
-        if call_result.is_err() {
-            env::panic_str("The was an error calling the remote contract");
-        }
-    
-        // Return the greeting
-        let has_usr: bool = call_result.unwrap();
-        
-        if has_usr {
-    
-            self.internal_create_collection(env::signer_account_id(), 
-            title, symbol, icon, 
-            base_uri, description, category, 
-            total_tickets,tickets_sold,
-            ticket_types, attributes, 
-            template_type, contract_id);
-    
-            self.date_updated = Some(env::block_timestamp());
-    
-        }
-        else {
-            env::panic_str(format!("User with '{}' does NOT exist",acc_id.as_str()).as_str());
-        }
-
-    }
-}
-
 
 #[near_bindgen]
 impl Contract {
